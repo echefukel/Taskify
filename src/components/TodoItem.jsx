867d3cc28df28a8d2e24c575b6fc60fc9043fd5f
Kelechi <kellymoor18@gmail.com>
2025-04-08 08:28:58 +0100

    flow corrections

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -4,11 +4,11 @@ export default function TodoItem({ todo, onToggle , onDelete }) {
     return (
       <li
         
-        className={`flex truncate justify-between items.center cursor-pointer border w-full p-4 border-gray-200 rounded-md bg-gray-100 shadow-md ${
+        className={`flex  justify-between items.center cursor-pointer border w-full p-4 border-gray-200 rounded-md bg-gray-100 shadow-md ${
           todo.done ? "line-through text-gray-500" : ""
         }`}
       >
-        <span onClick={onToggle} className="flex-1 cursor-pointer">{todo.text}</span>
+        <span onClick={onToggle} className="flex-1 truncate cursor-pointer">{todo.text}</span>
         <button onClick={onToggle} className="text-green-600">{todo.done ? "undo" : "Complete"}</button>
         <FaTrash className="ml-4 text-blue-500" onClick={onDelete} />
       </li>
